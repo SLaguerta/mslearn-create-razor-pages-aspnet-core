6e240c83967ee48828265ff87285c5f9f003270b
SLaguerta <20846911+SLaguerta@users.noreply.github.com>
2024-02-18 18:54:24 +0000

    Add NewPizza property and HTTP POST page handler to PizzaListModel

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -8,7 +8,12 @@ namespace ContosoPizza.Pages
     public class PizzaListModel : PageModel
     {
         private readonly PizzaService _service;
-        public IList<Pizza> PizzaList {get; set;} = default!;
+        public IList<Pizza> PizzaList { get; set; } = default!;
+        
+        //BindProperty attribute binds NewPizza property to the Razor Page
+        //When an HTTP POST request is made, NewPizza will be populated with user input from the page
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
 
         public PizzaListModel(PizzaService service)
         {
@@ -18,6 +23,20 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+
+        public IActionResult OnPost()
+        {
+            //ModelState.IsValid property validatess user input by checking against Pizza class model
+            if (!ModelState.IsValid || NewPizza == null){
+                return Page();
+            }
+
+            //NewPizza property is used to add new pizza to service object
+            _service.AddPizza(NewPizza);
+            
+            //Redirect use to Get page handler and rerender page with updated list of pizzas
+            return RedirectToAction("Get");
         }
     }
 }
